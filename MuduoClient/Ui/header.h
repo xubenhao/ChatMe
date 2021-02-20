fe6c8f5de8f38d76d81dd2994430e1455ec2c3b4
XuBenHao <xbh370970843@163.com>
2021-02-20 15:31:35 +0800

    ui

---
+++ MuduoClient/Ui/header.h
@@ -0,0 +1,6 @@
+#ifndef APP_UI_HEADER_H
+#define APP_UI_HEADER_H
+#include "Global/lib.h"
+#include "DataStruct/lib.h"
+#include "MuduoApp/lib.h"
+#endif // HEADER_H
---
+++ MuduoClient/Ui/lib.h
@@ -0,0 +1,5 @@
+#ifndef APP_UI_LIB_H
+#define APP_UI_LIB_H
+#include "mainwindow.h"
+#include "registerandloginwidget.h"
+#endif
