bd4ebe987e090b8b3adc1abb5b4a488885691c2c
XuBenHao <xbh370970843@163.com>
2021-02-20 15:23:40 +0800

    muduoapp

---
+++ MuduoClient/MuduoApp/header.h
@@ -0,0 +1,7 @@
+#ifndef MUDUO_APP_HEADER_H
+#define MUDUO_APP_HEADER_H
+#include "../MyMuduo/Tcp/lib.h"
+#include "../MyMuduo/Tcp/lib.h"
+#include "Global/lib.h"
+#include "DataStruct/lib.h"
+#endif // HEADER_H
---
+++ MuduoClient/MuduoApp/lib.h
@@ -0,0 +1,4 @@
+#ifndef MUDUO_APP_LIB_H
+#define MUDUO_APP_LIB_H
+#include "muduoclient.h"
+#endif // LIB_H
